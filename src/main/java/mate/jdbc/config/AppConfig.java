a6e101278f012819c655aa508d542cdce6e21455
OleksiiHubanov <oleksiyhubanov@gmail.com>
2023-05-18 17:22:17 +0300

    Taxi service. Commit #2.1. Spring connection, adding Config file.

---
+++ src/main/java/mate/jdbc/config/AppConfig.java
@@ -0,0 +1,17 @@
+package mate.jdbc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource("classpath:db.properties")
+@ComponentScan(basePackages = {"mate.jdbc.dao"})
+public class AppConfig {
+    private final Environment environment;
+
+    public AppConfig(Environment environment) {
+        this.environment = environment;
+    }
+}
